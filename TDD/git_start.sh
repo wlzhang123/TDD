67c147d1fb98db689829e98ed967e1a68198be50
wlzhang123 <weilinzhang60@hotmail.com>
2016-01-29 07:31:08 +0800

    Add comments into this git_start.sh

--- TDD/git_start.sh
+++ TDD/git_start.sh
@@ -1,3 +1,8 @@
+# set global user info
 git config --global user.name "wlzhang123"
 git config --golbal user.email "weilinzhang60@hotmail.com"
+# see which branches I have now
 git remote
+
+#git add_start.sh
+#git commit -m "add git script into a file"
