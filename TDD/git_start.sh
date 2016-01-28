aa2885968c152446e34e9043c324cb1d465bf6dc
wlzhang123 <weilinzhang60@hotmail.com>
2016-01-29 07:35:10 +0800

    add git push origin master

--- TDD/git_start.sh
+++ TDD/git_start.sh
@@ -4,6 +4,8 @@ git config --golbal user.email "weilinzhang60@hotmail.com"
 # see which branches I have now
 git remote
 
-#git add_start.sh
-#git commit -m "add git script into a file"
-#git commit -am "add comment into this git_start.sh"
+# git add_start.sh
+# git commit -m "add git script into a file"
+# git commit -am "add comment into this git_start.sh"
+## push local bransh origin to remote 
+# git push origin master
