500234658a88133e8232be9fd9c5cd54c126642d
wlzhang123 <weilinzhang60@hotmail.com>
2016-01-29 09:12:34 +0800

    commit git start copy back

--- TDD/git_start.sh
+++ TDD/git_start.sh
@@ -9,3 +9,13 @@ git remote
 # git commit -am "add comment into this git_start.sh"
 ## push local bransh origin to remote 
 # git push origin master
+
+## Multiple branches
+# git branch
+# git checkout harry
+# git checkout chapter_07
+# git pull
+# git checkout chapter_08
+# git pull
+## All source code will appear on  E:\run_test\TDD 
+## The same level with .git directory
